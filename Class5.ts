fbb7a9c641d9878a06c3ed9fb5ffe8c625d9909e
MuhammadSameer100 <162350271+MuhammadSameer100@users.noreply.github.com>
2024-03-05 22:04:16 +0500

    Add files via upload

---
+++ Class5.ts
@@ -0,0 +1,56 @@
+//let stdName="Sameer"
+//let isSlot="Tuesday"
+//let isAllowed = stdName==="Sameer" && isSlot==="Tuesday"
+// "Sameer"==="Sameer"//
+// true && true 
+//let is = true&&true
+//console.log(is);
+//face book example//
+//console.log(isAllowed)
+//let email = "Muhammadsameer"
+//let pass = "343"
+//let ia=email==="Muhammadsameer" && pass==="343"
+//console.log(ia)
+// or //
+//let firstvalue = true
+//let secondvalue = true
+//let ia = firstvalue || secondvalue
+//console.log(ia)
+// not "it reverse"//
+//console.log(!(5<4))
+
+// if and else condition//
+//if (5<4){console.log("if wala block");
+//code
+//}
+//else {
+//    console.log("else wala block")
+//}//
+//let isPass = true
+//if(isPass){
+//    console.log("Bike mil gahe")
+//}
+//else{
+//   console.log(riksa)}//
+
+/////if else 
+//let vehicle ="Bike"
+//if (vehicle==="Car"){
+//  console.log("Come") 
+//}
+//else if (vehicle==="Bike"){
+//   console.log("Come please")
+// }
+//else if (vehicle==="Trunk"){
+//   console.log("Do not come")
+//}
+
+let percentage = 45
+if (percentage >= 90) { console.log("You get A++") }
+else if (percentage >= 80) { console.log("You get A+ Grade") }
+else if (percentage >= 70) { console.log("You get A Grade") }
+else if (percentage >= 60) { console.log("You get B Grade") }
+else if (percentage >= 50) { console.log("You get C Grade") }
+else if (percentage >= 40) { console.log("You get D Grade") }
+else { console.log("You get F Grade") }
+
